30202ffc5dc51232b7c24d8d229844b8cea0d81c
Chaitanya Dusane <145824505+976333@users.noreply.github.com>
2026-08-09 18:27:17 +0530

    Time: 2100 ms (32.76%), Space: 42.1 MB (63.13%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        int prod = 0;
+        int temp = 1;
+
+        while((num/2)>=temp)
+        {
+            if(num%temp==0)
+            {
+                prod = prod+temp;
+            }
+            temp++;
+        }
+
+        if(prod==num)
+        {
+            return true;
+        }
+        else{
+        return false;
+        }
+     
+    }
+}
